fed05300e7d6b6c8c9d9fb342931bcf72788ac33
Leo Noailles <leo@nexio.info>
2026-03-30 16:16:10 +0200

    supp pocketbase + ajout chemin relatif pour admin

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 🟢 Ajoute cette ligne pour forcer les chemins absolus
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
