2cb66004238a439995f47e04348118f722b12381
Leo Noailles <leo@nexio.info>
2026-03-30 16:35:20 +0200

    securite variable env

--- web/src/lib/supabaseClient.js
+++ web/src/lib/supabaseClient.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nuczqhpetehnwtxhgciy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51Y3pxaHBldGVobnd0eGhnY2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NDk1ODQsImV4cCI6MjA5MDQyNTU4NH0.4GT5iqPxPeVWTLnyOw0jOeAQPVELLLCCf6GN89o7TnY'
+// 🟢 On récupère les clés depuis les variables d'environnement
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Les variables d'environnement Supabase sont manquantes !");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
